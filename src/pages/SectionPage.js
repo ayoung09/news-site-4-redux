49d01c7b6653f655190dec2a8bf7cfd095bf1f99
Alexia Young <alexia.kwok@gmail.com>
2017-12-03 11:47:27 -0600

    removing unnecessary import from section page

--- src/pages/SectionPage.js
+++ src/pages/SectionPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ArticleList from '../components/ArticleList/ArticleList.js'
-import ArticlesAPI from '../api/ArticlesAPI.js'
 import { fetchArticlesBySection } from '../modules/articles.module';
 
 const mapStateToProps = state => ({
